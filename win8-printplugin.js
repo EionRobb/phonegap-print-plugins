9e9e3a42581aa9b553e0d742a026d85731cfa3a6
Eion Robb <eion@robbmob.com>
2013-03-26 09:06:23 +1300

    Fix thread race in win8 version of plugin
    Sometimes the print preview would only display a blank page

--- win8-printplugin.js
+++ win8-printplugin.js
@@ -1,11 +1,14 @@
-var printManager = Windows.Graphics.Printing.PrintManager.getForCurrentView();
-function onPrintTaskRequested(printEvent) {
-  var printTask = printEvent.request.createPrintTask(document.title, function (args) {
-		args.setSource(MSApp.getHtmlPrintDocumentSource(document));
-	});
-	printManager.removeEventListener("printtaskrequested", onPrintTaskRequested);
-}
+
 window.print = function() {
-	printManager.addEventListener("printtaskrequested", onPrintTaskRequested);
+	var printManager = Windows.Graphics.Printing.PrintManager.getForCurrentView();
+	function onPrintTaskRequested(printEvent) {
+		var printTask = printEvent.request.createPrintTask(document.title, function (args) {
+			args.setSource(MSApp.getHtmlPrintDocumentSource(document));
+		});
+		printManager.removeEventListener("printtaskrequested", onPrintTaskRequested);
+	}
+	try {
+		printManager.addEventListener("printtaskrequested", onPrintTaskRequested);
+	} catch (e) {}
 	Windows.Graphics.Printing.PrintManager.showPrintUIAsync();
 }
