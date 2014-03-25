c4d00192a44985527f3d1a57966a63a864c6bf5d
Eion Robb <eion@robbmob.com>
2014-03-25 15:01:13 +1300

    Provide consistent API for print plugins on Win8
    Use the same API from iOS and Android on Metro/Win8 to make it easier to port between platforms

--- Metro/printplugin.js
+++ Metro/printplugin.js
@@ -11,3 +11,60 @@ window.print = function() {
 	} catch (e) {}
 	Windows.Graphics.Printing.PrintManager.showPrintUIAsync();
 }
+
+var PrintPlugin = function() {};
+
+PrintPlugin.prototype.print = function(printHTML, successCB, failCB, options) {
+	var document = window.document.createDocumentFragment();
+	var htmlElment = document.appendChild(window.document.createElement('html'));
+	MSApp.execUnsafeLocalFunction(function () {
+		htmlElment.innerHTML = printHTML;
+	});
+	
+	var printManager = Windows.Graphics.Printing.PrintManager.getForCurrentView();
+	function onPrintTaskRequested(printEvent) {
+		var printTask = printEvent.request.createPrintTask(options.title||document.title||"Print Document", function (args) {
+			args.setSource(MSApp.getHtmlPrintDocumentSource(document));
+			if (options.landscape)
+				printTask.options.orientation = Windows.Graphics.Printing.PrintOrientation.landscape;
+			printTask.oncompleted = function(printTaskCompletionEvent) {
+				if (printTaskCompletionEvent.completion === Windows.Graphics.Printing.PrintTaskCompletion.submitted) {
+					if (typeof successCB == "function")
+						successCB({'success':true});
+				} else {
+					var error = '';
+					switch(printTaskCompletionEvent.completion) {
+						case Windows.Graphics.Printing.PrintTaskCompletion.abandoned: error = "Abandoned"; break;
+						case Windows.Graphics.Printing.PrintTaskCompletion.canceled: error = "Cancelled"; break;
+						case Windows.Graphics.Printing.PrintTaskCompletion.failed: error = "Failed"; break;
+					}
+					if (typeof failCB == "function")
+						failCB({'error':error});
+				}
+			};
+		});
+		printManager.removeEventListener("printtaskrequested", onPrintTaskRequested);
+	}
+	try {
+		printManager.addEventListener("printtaskrequested", onPrintTaskRequested);
+	} catch (e) {}
+	Windows.Graphics.Printing.PrintManager.showPrintUIAsync();
+}
+
+PrintPlugin.prototype.isPrintingAvailable = function(callback) {
+	var dummyFunc = function(a){};
+	var available = true;
+	
+	try {
+		var printManager = Windows.Graphics.Printing.PrintManager.getForCurrentView();
+		printManager.addEventListener("printtaskrequested", dummyFunc);
+		printManager.removeEventListener("printtaskrequested", dummyFunc);
+	} catch(e) {
+		available = false;
+	}
+	
+	callback(available);
+};
+
+window.plugins = window.plugins || {};
+window.plugins.printPlugin = new PrintPlugin();
